9f7d3e455bb9ed7bf9c78a1d1a8b68fed448e9d9
Vishweshwaran <vishwa300499@gmail.com>
2020-10-15 18:31:06 +0530

    initial commit

---
+++ CTrack19/CTrack19App.swift
@@ -0,0 +1,17 @@
+//
+//  CTrack19App.swift
+//  CTrack19
+//
+//  Created by Vishweshwaran on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct CTrack19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
